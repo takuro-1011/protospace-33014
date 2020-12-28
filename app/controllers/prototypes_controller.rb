406c9cab067d4b76d30fb52c7ad2b99d864ef947
takuro-1011 <taku1011opal@icloud.com>
2020-12-28 16:51:00 +0900

    投稿者以外がeditアクションにアクセスしたらトップページにリダイレクトするように編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :edit]
   before_action :move_to_index, only: [:edit]
   def index
     @prototypes = Prototype.includes(:user)
